d189fdafabc0969901a151cda02dac834817c681
amitmishra.30032005 <amitmishra.30032005@gmail.com>
2024-09-21 15:15:37 +0000

    aaaaa

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-      <section className='px-6 md:px-20 py-2 border-2 border-red-500'>
+      <section className='px-6 border-b-0 md:px-20 py-2 border-2 border-red-500'>
         <div className='flex max-xl:flex-col gap-9'>
           <div className='flex flex-col justify-center'>
             <p className='small-text'>
