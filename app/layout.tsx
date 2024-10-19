ae0123befec80ed02e6038b5984f83a0670f7f79
Amit Mishra <113756289+amit738867@users.noreply.github.com>
2024-10-19 05:52:33 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className="max-w-10xl mx-auto">
-          <Navbar />
+{/*           <Navbar /> */}
           {children}
         </main>
       </body>
